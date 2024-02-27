7282f972813148b2e12ed30302c9727bb32c59ad
yeeun0118s <yeeun0118s@naver.com>
2024-02-27 10:14:16 +0900

    ã미로그인시 상세보기 ã안되는 ë²ë²디버깅 ì완료

--- src/main/java/shop/mtcoding/blog/board/BoardController.java
+++ src/main/java/shop/mtcoding/blog/board/BoardController.java
@@ -188,12 +188,20 @@ public class BoardController {
         boardDTO.isBoardOwner(sessionUser);
 
         List<BoardResponse.ReplyDTO> replyDTOList = replyRepository.findByBoardId(id, sessionUser);
-
         request.setAttribute("board", boardDTO);
         request.setAttribute("replyList", replyDTOList);
 
-        LoveResponse.DetailDTO loveDetailDTO = loveRepository.findLove(id, sessionUser.getId());
-        request.setAttribute("love", loveDetailDTO);
+        if(sessionUser == null){
+            LoveResponse.DetailDTO loveDetailDTO = loveRepository.findLove(id);
+            request.setAttribute("love", loveDetailDTO);
+        }else{
+            LoveResponse.DetailDTO loveDetailDTO = loveRepository.findLove(id, sessionUser.getId());
+            request.setAttribute("love", loveDetailDTO);
+        }
+
+        // fas fa-heart text-danger
+        // far fa-heart
+        // request.setAttribute("css", "far fa-heart");
 
         return "board/detail";
     }
--- src/main/java/shop/mtcoding/blog/love/LoveRepository.java
+++ src/main/java/shop/mtcoding/blog/love/LoveRepository.java
@@ -16,7 +16,31 @@ import java.util.List;
 public class LoveRepository {
     private final EntityManager em;
 
-    public LoveResponse.DetailDTO findLove(int boardId, int sessionUserId){
+    public LoveResponse.DetailDTO findLove(int boardId) {
+        String q = """
+                SELECT count(*) loveCount
+                FROM love_tb
+                WHERE board_id = ?;
+                """;
+        Query query = em.createNativeQuery(q);
+        query.setParameter(1, boardId);
+
+        // 한건만 받을때는 바로 받기
+        Long loveCount = (Long) query.getSingleResult();
+        Integer id = 0;
+        Boolean isLove = false;
+
+        System.out.println("id : " + id);
+        System.out.println("isLove : " + isLove);
+        System.out.println("loveCount : " + loveCount);
+
+        LoveResponse.DetailDTO responseDTO = new LoveResponse.DetailDTO(
+                id, isLove, loveCount
+        );
+        return responseDTO;
+    }
+
+    public LoveResponse.DetailDTO findLove(int boardId, int sessionUserId) {
         String q = """
                 SELECT
                     id,
@@ -35,14 +59,24 @@ public class LoveRepository {
         query.setParameter(2, boardId);
         query.setParameter(3, sessionUserId);
 
-        Object[] row = (Object[]) query.getSingleResult();
-        Integer id = (Integer) row[0];
-        Boolean isLove = (Boolean) row[1];
-        Long loveCount = (Long) row[2];
+        Integer id = null;
+        Boolean isLove = null;
+        Long loveCount = null;
+        try {
+            Object[] row = (Object[]) query.getSingleResult();
+            id = (Integer) row[0];
+            isLove = (Boolean) row[1];
+            loveCount = (Long) row[2];
+        }catch (Exception e){
+            id = 0;
+            isLove = false;
+            loveCount = 0L;
+        }
 
-        System.out.println("id : "+id);
-        System.out.println("isLove : "+isLove);
-        System.out.println("loveCount : "+loveCount);
+
+        System.out.println("id : " + id);
+        System.out.println("isLove : " + isLove);
+        System.out.println("loveCount : " + loveCount);
 
         LoveResponse.DetailDTO responseDTO = new LoveResponse.DetailDTO(
                 id, isLove, loveCount
